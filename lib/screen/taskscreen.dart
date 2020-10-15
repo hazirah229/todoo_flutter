3394c12be577e7fde577e31d5fe8d3bfae409383
hazirah229 <hazirah229isa@gmail.com>
2020-10-15 15:07:46 +0800

    update the UI

--- lib/screen/taskscreen.dart
+++ lib/screen/taskscreen.dart
@@ -3,8 +3,68 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      
+    return Scaffold(
+      backgroundColor: Colors.cyanAccent[700],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.cyanAccent[700],
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(
+                  top: 40, left: 30.0, right: 30.0, bottom: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30.0,
+                    child: Icon(
+                      Icons.list,
+                      color: Colors.cyanAccent[700],
+                      size: 30.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'ToDoo',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    '9 Tasks',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                //only shows up by having a child or set the dimension
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      topRight: Radius.circular(10.0)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
